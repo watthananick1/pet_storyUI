3c46e76404882b2cca6385637fe89bbe93b2fc9d
watthana ritthiphaeng <86664451+watthananick1@users.noreply.github.com>
2023-08-08 21:56:21 +0700

    Update Alert

---
+++ .history/src/components/dialogModel/dialogModel_20230808183934.jsx
@@ -0,0 +1,227 @@
+import React, { useState, useRef, useEffect } from "react";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Stack,
+} from "@mui/material";
+import AddAPhotoIcon from "@mui/icons-material/AddAPhoto";
+import { Send as SendIcon } from "@mui/icons-material";
+import { ReactCrop } from "react-image-crop";
+import "react-image-crop/dist/ReactCrop.css";
+import firebase from "firebase/compat/app";
+import "firebase/compat/storage";
+import LoadingButton from "@mui/lab/LoadingButton";
+import { Messageupdate } from "../../context/AuthActions";
+import { AuthContext } from "../../context/AuthContext";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+const storage = firebase.storage();
+const firestore = firebase.firestore();
+const path = process.env.REACT_APP_PATH_ID;
+
+export default function FilePreviewer({ onClose }) {
+  const [imagePreview, setImagePreview] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [crop, setCrop] = useState({
+    unit: "%",
+    x: 25,
+    y: 25,
+    width: 50,
+    height: 50,
+  });
+
+  const cropperRef = useRef(null);
+  const user = JSON.parse(localStorage.getItem("user"));
+
+  useEffect(() => {
+    return () => {
+      if (cropperRef.current) {
+        cropperRef.current.destroy();
+      }
+    };
+  }, []);
+
+  const handleCropChange = (newCrop) => {
+    setCrop(newCrop);
+  };
+
+  const handleClose = () => {
+    onClose();
+  };
+
+  const handleCancel = () => {
+    setImagePreview(null);
+    handleClose();
+  };
+
+  const previewFile = (e) => {
+    const reader = new FileReader();
+    const selectedFile = e.target.files[0];
+
+    if (selectedFile) {
+      reader.readAsDataURL(selectedFile);
+      reader.onloadend = () => {
+        setImagePreview(reader.result);
+      };
+    }
+  };
+
+  const getCroppedImageBlob = async (sourceImageUrl, crop) => {
+    const image = new Image();
+    image.src = sourceImageUrl;
+
+    return new Promise((resolve) => {
+      image.onload = () => {
+        const canvas = document.createElement("canvas");
+        const scaleX = image.naturalWidth / image.width;
+        const scaleY = image.naturalHeight / image.height;
+        canvas.width = crop.width;
+        canvas.height = crop.height;
+        const ctx = canvas.getContext("2d");
+
+        const pixelRatio = window.devicePixelRatio;
+        canvas.width = crop.width * pixelRatio;
+        canvas.height = crop.height * pixelRatio;
+        ctx.setTransform(pixelRatio, 0, 0, pixelRatio, 0, 0);
+        ctx.imageSmoothingQuality = "high";
+
+        ctx.drawImage(
+          image,
+          crop.x * scaleX,
+          crop.y * scaleY,
+          crop.width * scaleX,
+          crop.height * scaleY,
+          0,
+          0,
+          crop.width,
+          crop.height
+        );
+
+        canvas.toBlob((blob) => {
+          resolve(URL.createObjectURL(blob)); // Convert the blob to a URL
+        }, "image/jpeg");
+      };
+    });
+  };
+
+  const handleAvatarUpload = async () => {
+    setLoading(true);
+
+    try {
+      const storageRef = storage.ref();
+      const fileName = `${Date.now()}_profile`;
+      const fileRef = storageRef.child(
+        `${user.member_id}/profilePicture/${fileName}`
+      );
+
+      if (!imagePreview) {
+        throw new Error("No image selected");
+      }
+
+      const croppedImage = await getCroppedImageBlob(imagePreview, crop);
+
+      if (!croppedImage) {
+        throw new Error("Error cropping image");
+      }
+
+      const response = await fetch(croppedImage);
+      const blob = await response.blob();
+
+      const file = new File([blob], fileName, { type: blob.type });
+
+      const uploadTaskSnapshot = await fileRef.put(file);
+
+      const downloadUrl = await uploadTaskSnapshot.ref.getDownloadURL();
+
+      await firestore.collection("Users").doc(user.member_id).update({
+        profilePicture: downloadUrl,
+      });
+
+      //console.log("Profile picture updated successfully:", downloadUrl);
+
+      onClose();
+    } catch (error) {
+      console.log("Error uploading profile picture:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div>
+      <Dialog open={true} onClose={handleClose}>
+        <DialogTitle>Update Profile Picture</DialogTitle>
+        <DialogContent>
+          <Stack spacing={2}>
+            <Stack
+              direction="row"
+              spacing={2}
+              alignItems="center"
+              justifyContent="center"
+              mt={2}
+            >
+              <input
+                accept="image/png, image/jpeg, image/jpg"
+                onChange={previewFile}
+                ref={cropperRef}
+                type="file"
+                hidden
+              />
+
+              <Button
+                variant="outlined"
+                className="btn"
+                startIcon={<AddAPhotoIcon />}
+                onClick={() => cropperRef.current.click()}
+              >
+                Choose
+              </Button>
+            </Stack>
+            <Stack direction="row" spacing={2} alignItems="center" mt={2}>
+              {imagePreview && (
+                <ReactCrop
+                  crop={crop}
+                  onChange={handleCropChange}
+                >
+                  <>
+                    <img src={imagePreview} />
+                  </>
+                </ReactCrop>
+              )}
+            </Stack>
+          </Stack>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCancel}>Cancel</Button>
+          <LoadingButton
+            onClick={handleAvatarUpload}
+            variant="outlined"
+            color="success"
+            startIcon={<SendIcon />}
+            className="shareButton"
+            loading={loading}
+            disabled={loading}
+          >
+            Submit
+          </LoadingButton>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
---
+++ .history/src/pages/login/Login_20230808213151.jsx
@@ -0,0 +1,294 @@
+import { useState, useContext, forwardRef, useEffect } from "react";
+import "./login.css";
+import { loginCall } from "../../apiCalls";
+import { AuthContext } from "../../context/AuthContext";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import { Link } from "react-router-dom";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useHistory } from "react-router";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import IconButton from "@mui/material/IconButton";
+import InputAdornment from "@mui/material/InputAdornment";
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert from "@mui/material/Alert";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const Alert = forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
+function Copyright(props) {
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
+      <Link color="inherit" href="">
+        Pet story
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+const theme = createTheme();
+
+export default function SignInSide() {
+  const { error: isOpen, dispatch } = useContext(AuthContext);
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  const [open, setOpen] = useState(false);
+  const [message, setMessage] = useState({ severity: "success", text: " " });
+  const history = useHistory();
+  const [rememberMe, setRememberMe] = useState(false);
+
+  // const handleClick = () => {
+  //   setOpen(true);
+  // };
+
+  useEffect(() => {
+    setOpen(isOpen);
+  }, [isOpen]);
+
+  const handleFacebookLogin = async () => {
+    const provider = new firebase.auth.FacebookAuthProvider();
+
+    try {
+      const result = await firebase.auth().signInWithPopup(provider);
+      // เข้าสู่ระบบสำเร็จ
+      console.log("Facebook login successful", result.user);
+    } catch (error) {
+      console.error("Facebook login error:", error);
+    }
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+
+    try {
+      await loginCall(
+        { email: data.get("email"), password: data.get("password") },
+        dispatch
+      );
+      setMessage({
+        severity: "error",
+        text: "Please fill out all fields correctly",
+      });
+      history.push("/");
+    } catch (error) {
+      setMessage({ severity: "error", text: "An error occurred: " + error });
+      setOpen(true);
+      console.error("Error:", error);
+    }
+  };
+
+  const handleChangeRememberMe = () => {
+    setRememberMe(!rememberMe);
+  };
+
+  return (
+    <div className="login">
+      <div className="loginWrapper">
+        <ThemeProvider theme={theme}>
+          <Snackbar
+            open={open}
+            autoHideDuration={6000}
+            onClose={handleClose}
+            anchorOrigin={{ vertical: "top", horizontal: "right" }}
+          >
+            <Alert
+              onClose={handleClose}
+              severity={message.severity}
+              sx={{ width: "100%" }}
+            >
+              {message.text}
+            </Alert>
+          </Snackbar>
+          <Grid container component="main">
+            <CssBaseline />
+            <Grid
+              item
+              xs={false}
+              sm={4}
+              md={7}
+              sx={{
+                backgroundImage:
+                  "url(https://firebasestorage.googleapis.com/v0/b/pet-story-f51e3.appspot.com/o/mainData%2FloginImage1.jpg?alt=media&token=4547e22e-e032-4eb9-82fa-84551789d27e)",
+                backgroundRepeat: "no-repeat",
+                backgroundColor: (t) =>
+                  t.palette.mode === "light"
+                    ? t.palette.grey[50]
+                    : t.palette.grey[900],
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+              }}
+            />
+            <Grid
+              item
+              xs={12}
+              sm={8}
+              md={5}
+              component={Paper}
+              elevation={6}
+              square
+            >
+              <Box
+                sx={{
+                  my: 8,
+                  mx: 4,
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                }}
+              >
+                <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+                  <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                  Sign in
+                </Typography>
+                <Box
+                  component="form"
+                  noValidate
+                  onSubmit={handleSubmit}
+                  sx={{ mt: 1 }}
+                >
+                  <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="email"
+                    label="Email Address"
+                    name="email"
+                    autoComplete="email"
+                    autoFocus
+                  />
+                  <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type={passwordVisible ? "text" : "password"}
+                    id="password"
+                    autoComplete="current-password"
+                    InputProps={{
+                      endAdornment: (
+                        <InputAdornment position="end">
+                          <IconButton
+                            aria-label="toggle password visibility"
+                            onClick={() => setPasswordVisible(!passwordVisible)}
+                            edge="end"
+                          >
+                            {passwordVisible ? (
+                              <VisibilityOff />
+                            ) : (
+                              <Visibility />
+                            )}
+                          </IconButton>
+                        </InputAdornment>
+                      ),
+                    }}
+                  />
+                  <FormControlLabel
+                    control={
+                      <Checkbox
+                        value="remember"
+                        color="primary"
+                        checked={rememberMe}
+                        onChange={handleChangeRememberMe}
+                      />
+                    }
+                    label="Remember me"
+                  />
+                  <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2 }}
+                  >
+                    Sign In
+                  </Button>
+                  <Grid
+                    container
+
+                    elevation={6}
+                    square
+                  >
+                    <Grid item xs>
+                      <div className="divider">
+                        <hr className="divider-line" />
+                        <span className="divider-text">OR</span>
+                        <hr className="divider-line" />
+                      </div>
+                    </Grid>
+                    <Grid item>
+                      <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        onClick={handleFacebookLogin}
+                        sx={{ mt: 3, mb: 2 }}
+                      >
+                        Login with Facebook
+                      </Button>
+                    </Grid>
+                  </Grid>
+                  <Grid container>
+                    <Grid item xs>
+                      <Link to="/reset_password" variant="body2">
+                        Forgot password?
+                      </Link>
+                    </Grid>
+                    <Grid item>
+                      <Link to="/register" variant="body2">
+                        {"Don't have an account? Sign Up"}
+                      </Link>
+                    </Grid>
+                  </Grid>
+                  <Copyright sx={{ mt: 5 }} />
+                </Box>
+              </Box>
+            </Grid>
+          </Grid>
+        </ThemeProvider>
+      </div>
+    </div>
+  );
+}
